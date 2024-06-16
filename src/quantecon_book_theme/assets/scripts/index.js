fe019087758a33509ec35f1d660ed05600873f97
huhuhang <no-reply@huhuhang.com>
2024-06-16 15:21:57 +0800

    fix

--- src/quantecon_book_theme/assets/scripts/index.js
+++ src/quantecon_book_theme/assets/scripts/index.js
@@ -203,9 +203,9 @@ document.addEventListener("DOMContentLoaded", function () {
   function setFontSize() {
     // Get font size from local storage
     var toolbarFont = localStorage.toolbarFont;
-    // 默认值为 -1
+    // 默认值为 1
     if (toolbarFont == null) {
-      toolbarFont = -1;
+      toolbarFont = 1;
     }
     if (toolbarFont == 1) {
       $("html").addClass("font-plus");
